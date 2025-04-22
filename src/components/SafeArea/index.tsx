fd6a8415cee29232b842ea421c23d816dc841d4c
Merdan-Mahmudow <softprank@gmail.com>
2025-04-23 01:34:04 +0300

    modified:   Шторка от capcitor с применением темы 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/SafeArea/index.tsx 	modified:   src/main.tsx

--- src/components/SafeArea/index.tsx
+++ src/components/SafeArea/index.tsx
@@ -1,9 +1,28 @@
-import { MotionBox } from "../../motion";
+import { useEffect } from 'react';
+import { useColorMode } from '../ui/color-mode';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
-export function SafeArea() {
-    return (
-        <MotionBox p={"2em"}>
-            
-        </MotionBox>
-    )
-}
+export const StatusBarSync = () => {
+  const { colorMode } = useColorMode();
+
+  useEffect(() => {
+    console.log(colorMode)
+    const applyStatusBarStyle = async () => {
+      // Тёмная тема: делаем статус-бар светлым (белый фон, тёмные иконки)
+      if (colorMode === 'light') {
+        await StatusBar.setStyle({ style: Style.Dark });
+        await StatusBar.setBackgroundColor({ color: '#ffffff' });
+      } else {
+        // Светлая тема: делаем статус-бар тёмным (тёмный фон, светлые иконки)
+        await StatusBar.setStyle({ style: Style.Light });
+        await StatusBar.setBackgroundColor({ color: '#1A202C' });
+      }
+
+      await StatusBar.setOverlaysWebView({ overlay: false });
+    };
+
+    applyStatusBarStyle();
+  }, [colorMode]);
+
+  return null; 
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
 import { Provider } from './components/ui/provider.tsx'
 import './index.css'
+import { StatusBarSync } from './components/SafeArea/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider >
+      <StatusBarSync />
       <App />
     </Provider>
   </StrictMode>,
